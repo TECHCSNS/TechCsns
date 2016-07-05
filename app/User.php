270272a8f5b8294167d7508bb06db16bec8681bb
Chan You Chun <ilvinchan95@hotmail.com>
2016-07-05 06:39:28 +0000

    バッグ解決完了

--- app/User.php
+++ app/User.php
@@ -24,6 +24,6 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     public function board(){
-        return $this->hasMany('App\Board')
+        return $this->hasMany('App\Board');
     }
 }
